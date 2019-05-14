8417fb6aa0114779f47aaa3f3a52fb406786da49
willow <shaneweth77@gmail.com>
2019-05-14 17:07:46 -0400

    changes to server.js to allow port to be decided by Heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require("express");
 
+
 var app = express();
+
+var PORT = process.env.PORT;
 
 app.get("/", function(req, res){
 
     res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000);
+app.listen(PORT || 3000);
 
