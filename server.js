d5d1c4c6514c4bef16a5dd3edc8004f1c9026168
willow <shaneweth77@gmail.com>
2019-05-14 13:29:05 -0400

    added the server.js file and express route. combined js and css into index.html

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-var http = require("http");
-var fs = require("fs");
+var express = require("express");
 
+var app = express();
 
-var server = http.createServer(function(req, res) {
-    console.log("request made: " + req.url);
-    res.writeHead(200, {"Content-Type": "text/html"});
-    var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
-    myReadStream.pipe(res);
+app.get("/", function(req, res){
+
+    res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(3000, "127.0.0.1");
-console.log("heloo on port 3000");
+app.listen(3000);
+
